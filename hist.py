19be75fdc9629b47c1c1c06d646bc10dda319c72
Alex Hakso <alex.hakso@gmail.com>
2016-10-14 16:35:14 -0700

    no major change

--- hist.py
+++ hist.py
@@ -1,7 +1,13 @@
 import csv
+from datetime import datetime
+date = []
+rownum = 0
 with open ('sampleHistory.csv') as csvfile:
 	amzreader = csv.DictReader(csvfile,delimiter=',')
 	amzreader
- 	for row in amzreader:
+ 	for row in amzreader:	
+		date.append(datetime.strptime(row['Order Date'], '%m/%d/%y'))
  		#print(row['Order Date'])
-		print type(row['Order Date'])
+		#print type(row['Order Date'])
+		rownum = rownum+1
+	print row[0]
